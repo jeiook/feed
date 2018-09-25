1667d6eada886c6b04255aad4a98f4bc5970fc72
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2018-09-24 18:33:52 -0700

    changes 9/24
    Completed initial design; need to remove horizontal scrolling and do responsive design.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
 $(document).ready(function() {
+	/*var size = [window.width,window.height];
+	if (window.width <= 320) {
+		$(window).resize(function() {
+	    	window.resizeTo(size[0],size[1]);
+		});
+	}*/
 });
